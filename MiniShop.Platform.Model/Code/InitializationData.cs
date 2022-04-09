c6d09987fd16ccf091714af75aa46748338bfe4f
weichangk <18276743761@163.com>
2022-04-09 15:31:42 +0800

    add

---
+++ MiniShop.Platform.Model/Code/InitializationData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace MiniShop.Platform.Model.Code
+{
+    public class InitializationData
+    {
+        public List<RenewPackage> RenewPackage { get; set; }
+        public static InitializationData Initialization { get; set; } = new InitializationData();
+    }
+}
